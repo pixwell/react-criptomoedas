b51e6dfc59f4e11e21d76d83dd3a65deaecf470f
Marcia Silva <contato.pix@gmail.com>
2026-09-03 14:50:59 -0300

    Estilização CSS

--- src/pages/pageNotFound/index.tsx
+++ src/pages/pageNotFound/index.tsx
@@ -1,7 +1,7 @@
 
 export function PageNotFound() {
   return (
-    <div className="container text-center py-10">
+    <div className="container text-center">
       <h1 className="text-6xl">404</h1>
       <h2 className="text-3xl">Página não encontrada!</h2>
       <p className="text-center">Página ou conteúdo não encontrado.</p>
