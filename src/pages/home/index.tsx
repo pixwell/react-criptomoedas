146abfd10b5cc0b08ec5045f3e8c8de45c6d586d
Marcia Silva <contato.pix@gmail.com>
2026-09-03 16:53:43 -0300

    Estilização do formulário de busca

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
-
+import { FaSearch } from "react-icons/fa";
 
 export function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="container">
+      <form className="flex justify-between items-center gap-6">
+        <input type="text" name="coin-name" placeholder="Digite o nome da moeda. Ex.: Bitcoin" className="bg-white text-zinc-500 w-full rounded-md px-4 py-2" />
+        <button type="submit">
+          <FaSearch size={28} color="#E4E4E7"/>
+        </button>
+      </form>
+
+      
     </div>
   )
 }
