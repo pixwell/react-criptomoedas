271ebf753982612a664387442a2eca57249e5ece
Marcia Silva <contato.pix@gmail.com>
2026-09-03 21:16:24 -0300

    Alteração no nome da página

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@
 export function Header() {
   return (
     <div className="container py-10">
-      <h1 className="text-5xl text-center"><span className="text-blue-800 text-bold">Dev</span>Bitcoin</h1>
+      <h1 className="text-5xl text-center"><span className="text-blue-800 text-bold">Dev</span>Cripto</h1>
     </div>
   )
 }
