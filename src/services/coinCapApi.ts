d970a99eb07bb5d575b7c8b33ce9136a7dd23aa5
Marcia Silva <contato.pix@gmail.com>
2026-09-09 16:49:46 -0300

    Criada a constante ITEMS_PER_PAGE para conter o valor padrão de items por página na propriedade limit

--- src/services/coinCapApi.ts
+++ src/services/coinCapApi.ts
@@ -1,13 +1,15 @@
 import type { CoinResponse } from "../types/coin";
 
-function getApiUrl(endpoint: string = 'assets', limit: number = 10, offset: number = 0): string{
+export const ITEMS_PER_PAGE = 10;
+
+function getApiUrl(endpoint: string = 'assets', limit: number = ITEMS_PER_PAGE, offset: number = 0): string{
   return `https://rest.coincap.io/v3/${endpoint}?limit=${limit}&offset=${offset}&apiKey=${import.meta.env.VITE_COINCAP_API_KEY}`;
 }
 
 export async function getCoins(offset: number = 0): Promise<CoinResponse>{
   try {
     //Requisicao inicial
-    const response = await fetch(getApiUrl('assets', 10, offset));
+    const response = await fetch(getApiUrl('assets', ITEMS_PER_PAGE, offset));
     //Trata o erro 404, que nao e tratado nativamente
     if (!response.ok) {
       throw new Error(`Erro HTTP: ${response.status}`);
