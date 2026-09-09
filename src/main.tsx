aaa75e6fbede0c2dc99763fb4f135954e57570d3
Marcia Silva <contato.pix@gmail.com>
2026-09-09 15:43:23 -0300

    Instalação e configuração do pacote React Hot Toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from "./App";
 import { RouterProvider } from "react-router/dom";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster position="top-center" reverseOrder={false} />
     <RouterProvider router={router} />
   </StrictMode>,
 )
