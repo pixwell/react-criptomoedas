f0ce2406a2a2bbee9d202cb20b53717b55e81240
Marcia Silva <contato.pix@gmail.com>
2026-09-10 17:05:31 -0300

    Criada a função formatadora para valores monetários comuns, formatPrice()

---
+++ src/utils/formatters.ts
@@ -0,0 +1,9 @@
+
+export function formatPrice(value: number): string{
+    const price = Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(value)
+
+    return price;
+}
